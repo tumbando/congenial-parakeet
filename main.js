df63fd4000e147fb629bb33139fa79f63838c078
Louie Alarcon <tumbando@gmail.com>
2016-06-30 14:50:34 -0500

    created hidden class, used jquery to toggle between adding and removing class

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-$('.tab').on('click', function(){
-  
-});
+  $('.tab').on('click', function(evt){
+    $('ol').toggleClass('hidden');
+    $('li').toggleClass('hidden');
+    $('.counter').toggleClass('hidden');
+  });
+
+//   var $tab=$('.tab');
+// $tab.on('click', function(){
+//   $('.container').slideDown();
+// });
 });
