b022f4fa4a03f51b57809c438a72db3b7fa68753
Louie Alarcon <tumbando@gmail.com>
2016-07-01 15:13:52 -0500

    restructred html layout

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-$(document).ready(function() {
-  $('.tab').on('click', function(evt){
-    $('ol').slideUp();
-      // $('.counter').toggleClass('hidden');
-  });
-
-//   var $tab=$('.tab');
-// $tab.on('click', function(){
-//   $('.container').slideDown();
+// $(document).ready(function() {
+//     $('.tab').on('click', function(evt) {
+//         $('ol').addClass('hidden');
+//         $(this).children('ol').removeClass('hidden');
+//
+//         //   var $tab=$('.tab');
+//         // $tab.on('click', function(){
+//         //   $('.container').slideDown();
+//         // });
+//     });
 // });
-});
+//
+//
+//
+// //when i click on ANY tab, give ALL content hidden class,
+// //then remove it from whatever tab was clicked.
